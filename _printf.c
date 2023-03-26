ccfb7512f292f8e6763dc051891e8cf4b074d342
sayedhany <sayedh@mail.ru>
2023-03-26 14:23:57 -0700

    finished

--- _printf.c
+++ _printf.c
@@ -1,83 +1,48 @@
 #include "main.h"
+#include <limits.h>
 #include <stdio.h>
-#include <unistd.h>
-#include <stdarg.h>
-#include <stdlib.h>
+
 /**
- * _printf - our own printf
- * @format: string
- * Return: number of prints
+ * _printf - produces output according to a format
+ * @format: format string containing the characters and the specifiers
+ * Description: this function will call the get_print()
+ * Return: length of the formatted output string
  */
 int _printf(const char *format, ...)
 {
-	int i = 0, count = 0, j = 0;
-	va_list args;
-	char c;
-	char *str;
-	int inum;
-	char num_str[32];
-	char minus = '-';
+	int (*pfunc)(va_list, flags_t *);
+	const char *p;
+	va_list arguments;
+	flags_t flags = {0, 0, 0};
 
-	va_start(args, format);
-	if (format == NULL)
+	register int count = 0;
+
+	va_start(arguments, format);
+	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
-	while (format[i] != '\0')
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = format; *p; p++)
 	{
-		if (format[i] == '%')
+		if (*p == '%')
 		{
-			i++;
-			switch (format[i])
+			p++;
+			if (*p == '%')
 			{
-				case 'c':
-				  c = va_arg(args, int);
-					write(1, &c, sizeof(char));
-					i++;
-					count++;
-					break;
-				case 's':
-					str = va_arg(args, char *);
-					while (*str != '\0')
-					{
-						write(1, str, 1);
-						str++;
-						count++;
-					}
-					i++;
-					break;
-				case '%':
-					write(1, "%", 1);
-					i++;
-					count++;
-					break;
-				case 'd':
-				case 'i':
-					inum = va_arg(args, int);
-					if (inum < 0)
-					{
-						inum *= -1;
-						write(1, &minus, 1);
-					}
-					do {
-						num_str[j++] = inum % 10 + '0';
-						inum /= 10;
-					} while (inum != 0);
-					while (j > 0)
-					{
-						write(1, &num_str[--j], 1);
-					}
-					i++;
-					count++;
-					break;
-				default:
-					return (-1);
+				count += _putchar('%');
+				continue;
 			}
+			while (get_flag(*p, &flags))
+				p++;
+			pfunc = get_print(*p);
+			count += (pfunc)
+				? pfunc(arguments, &flags)
+				: _printf("%%%c", *p);
 		} else
-		{
-			write(1, &format[i], 1);
-			i++;
-			count++;
-		}
+			count += _putchar(*p);
 	}
-	va_end(args);
+	_putchar(-1);
+	va_end(arguments);
 	return (count);
+
 }
