603b98d6a1aaac71e646c0b4c2edbfaf743a7158
sayedhany <sayed591999@gmail.com>
2023-03-26 03:42:05 +0200

    solved

--- _printf.c
+++ _printf.c
@@ -17,7 +17,7 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 	if (format == NULL)
-		return (0);
+		return (-1);
 	while (format[i] != '\0')
 	{
 		if (format[i] == '%')
@@ -47,7 +47,7 @@ int _printf(const char *format, ...)
 					count++;
 					break;
 				default:
-					return (0);
+					return (-1);
 			}
 		} 
 		else
